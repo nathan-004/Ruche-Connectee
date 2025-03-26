9f093ca9f0563ceb7b7d547aed8ff066da1a6925
Nathan <148985551+nathan-004@users.noreply.github.com>
2025-03-26 20:32:13 +0100

    Add files via upload

---
+++ Reconnaissance-sonore/tests.py
@@ -0,0 +1,15 @@
+class Tests():
+
+    # Resultats de l'audio attaqueFrelons_abeilles.mp3 sous forme d'un dictionnaire de (debut, fin): true
+    abeilles_frelons = {
+        ("0", "0.06"): True,
+        ("0.45", "0.55"): False,
+        ("1.02", "1.08"): True,
+        ("1.15", "1.17"): True,
+        ("1.35", "1.54"): False,
+        ("1.55", "2.05"): True,
+        ("2.14", "2.19"): True,
+        ("2.25", "2.30"): True,
+        ("2.34", "2.40"): True,
+        ("2.56", "3.08"): True,
+    }
