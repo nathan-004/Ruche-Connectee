62ea5a6c4a0bbcee0985b5b9b9fc11c326ea1ceb
Nathan <148985551+nathan-004@users.noreply.github.com>
2025-04-05 17:39:46 +0200

    Update main.py

--- Microbit-Ruche/main.py
+++ Microbit-Ruche/main.py
@@ -2,19 +2,37 @@ def get_state(pin: number):
     # Humidite + température
     return pins.analog_read_pin(pin)
 
-def get_date():
-    ms = input.running_time()
-    # Modifier saison
-    return [0, 0, 0, 0, 0]
+def convert(t, cur, res): # indexs
+    calc = [1000, 60, 60, 24, 30] # ms -> s -> m -> h -> j
+    if cur > res:
+        for i in range(cur-1, res-1, -1):
+            t *= calc[i]
+    elif cur < res:
+        for i in range(cur, res, 1):
+            t /= calc[i]
+    return t
 
-# Envoyer signal avec le réseau LoRaWAN
-def envoyer_message():
+def get_date():
+    ms = input.running_time() # Nombre de ms depuis le lancement du programme
+
+    date = []
+
+    for i in range(4, -1, -1):
+        date.append(convert(ms, 0, i))
+        ms -= convert(date[-1], i, 0)
+
+    if date[0] != first_date[0]:
+        saison = saisons[date[0] - 1]
+
+    return date
+
+
+def envoyer_message(): # Envoyer signal avec le réseau LoRaWAN
     pass
 
+first_date = [4, 5, 9, 16, 0] # m, j, h, m, s
 
-first_date = (4, 5, 9, 16, 0) # m, j, h, m, s
-
-saisons = ["hiver",
+saisons = ("hiver",
     "hiver",
     "hiver",
     "printemps",
@@ -26,23 +44,21 @@ saisons = ["hiver",
     "automne",
     "automne",
     "automne"
-]
+)
 
-saison = saisons[first_date[0] + 1]
+saison = saisons[first_date[0] - 1]
 
 def on_forever():
     date = get_date()
     temp = get_state(AnalogPin.P0)
-    hum = get_state(AnalogPin.P1)
-    # ----------------------------------Mettre nom du pin
+    hum = get_state(AnalogPin.P1) # ----------------------------------Mettre nom du pin
     if saison == "automne" or saison == "hiver":
-        if temp <= 0:
-            # Modifier valeur
+        if temp <= 0: # Modifier valeur
             envoyer_message()
         if hum >= 65:
             envoyer_message()
     else:
-        if temp <= 0:
+        if temp <= 0: # Modifier valeur
             envoyer_message()
         if hum >= 80:
             envoyer_message()
