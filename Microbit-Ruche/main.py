ecbea4cf19d31a1e6795aee8a92b78528fc57919
Nathan <148985551+nathan-004@users.noreply.github.com>
2025-04-06 10:05:49 +0200

    Update main.py

--- Microbit-Ruche/main.py
+++ Microbit-Ruche/main.py
@@ -1,3 +1,25 @@
+def list(iterable):
+    t = []
+    
+    for el in iterable:
+        t.append(el)
+
+    return t
+
+def round(n):
+    # Round to the lowest integer
+
+    N = str(n)
+    N1 = list(N)
+
+    if "." not in N1:
+        return n
+
+    return int(N[:N1.index_of(".")])
+
+def enumerate(iterable):
+    return [(i, iterable[i]) for i in range(len(iterable))]
+
 def get_state(pin: number):
     # Humidite + température
     return pins.analog_read_pin(pin)
@@ -10,22 +32,33 @@ def convert(t, cur, res): # indexs
     elif cur < res:
         for i in range(cur, res, 1):
             t /= calc[i]
-    return t
+    return round(t)
 
 def get_date():
     ms = input.running_time() # Nombre de ms depuis le lancement du programme
-
     date = []
 
+    j = 0
     for i in range(4, -1, -1):
         date.append(convert(ms, 0, i))
-        ms -= convert(date[-1], i, 0)
+        ms -= convert(date[j], i, 0)
+        j+=1
 
-    if date[0] != first_date[0]:
+    # Addition de first_date et de date
+    max_ = [1000, 60, 60, 24, 30, 12]
+    new_date = []
+
+    for t in enumerate(date):
+        idx, val = t
+        res = val + first_date[idx]
+        if res > max_[idx]:
+            res %= max_[idx]
+        new_date.append(res)
+
+    if new_date[0] != first_date[0]:
         saison = saisons[date[0] - 1]
 
-    return date
-
+    return new_date
 
 def envoyer_message(): # Envoyer signal avec le réseau LoRaWAN
     pass
@@ -45,11 +78,11 @@ saisons = ("hiver",
     "automne",
     "automne"
 )
-
 saison = saisons[first_date[0] - 1]
 
 def on_forever():
     date = get_date()
+    serial.write_numbers(date)
     temp = get_state(AnalogPin.P0)
     hum = get_state(AnalogPin.P1) # ----------------------------------Mettre nom du pin
     if saison == "automne" or saison == "hiver":
