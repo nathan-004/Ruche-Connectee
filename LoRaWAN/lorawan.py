b3dfb271eb8f8d51ba489060de32b63389c92426
Nathan <148985551+nathan-004@users.noreply.github.com>
2025-05-11 16:20:22 +0200

    Update lorawan.py

--- LoRaWAN/lorawan.py
+++ LoRaWAN/lorawan.py
@@ -1,47 +1,63 @@
 from microbit import *
-import time
+import serial
 
-# Utilisation de l'UART pour communiquer avec le module Wio-E5
-uart.init(baudrate=9600, bits=8, parity=None, stop=1)
+# Remplace par tes clés TTN
+DevEUI  = "0123456789ABCDEF"
+AppEUI  = "0011223344556677"
+AppKey  = "AABBCCDDEEFF00112233445566778899"
 
-# Infos TTN (à modifier avec les vôtres)
-DevEUI = "XXXXXXXXXXXXXXXX"
-AppEUI = "YYYYYYYYYYYYYYYY"
-AppKey = "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ"
+# Rediriger l'UART sur P0 (TX) et P1 (RX) à 9600 bauds
+serial.redirect(SerialPin.P0, SerialPin.P1, 9600)
 
-# Fonction pour envoyer une commande AT et attendre la réponse
-def send_at(cmd, delay=2000):
-    uart.write((cmd + '\r\n'))
-    time.sleep_ms(delay)
-    response = uart.read()
-    if response:
-        print(response.decode('utf-8'))
+# Envoie une commande AT et attend la réponse
+def send_at(cmd):
+    serial.write_string(cmd + "\r\n")
+    basic.pause(2000)
+    resp = serial.read()
+    if resp:
+        basic.show_string("R")   # Réponse reçue
     else:
-        print("Pas de réponse.")
+        basic.show_string("E")   # Erreur / pas de réponse
 
-# Initialisation LoRa
+# Initialisation du module Wio-E5 pour TTN
 def init_lora():
-    display.show('I')  # Init
-    send_at('AT+ID=DevEUI,"{}"'.format(DevEUI))
-    send_at('AT+ID=AppEUI,"{}"'.format(AppEUI))
-    send_at('AT+KEY=APPKEY,"{}"'.format(AppKey))
-    send_at('AT+DR=EU868')  # Région
-    send_at('AT+MODE=LWOTAA')  # Activation via OTAA
-    send_at('AT+JOIN')  # Rejoindre le réseau
-    display.show('J')  # Join
-    time.sleep(10)
+    basic.show_string("I")  # Init
+    send_at("AT")      # Test de vie
+    send_at("AT+ID=DevEUI,\""  + DevEUI + "\"")
+    send_at("AT+ID=AppEUI,\""  + AppEUI + "\"")
+    send_at("AT+KEY=APPKEY,\"" + AppKey + "\"")
+    send_at("AT+DR=EU868")
+    send_at("AT+MODE=LWOTAA")
+    send_at("AT+JOIN")
+    basic.show_string("J")  # Join
+    basic.pause(10000)
 
-# Envoi d’un message
-def send_message(payload):
-    # Envoyer en format hexadécimal
-    send_at('AT+MSGHEX="{}"'.format(payload.encode('utf-8').hex()))
+def char_to_hex(c):
+    # Simuler ord() pour les lettres et chiffres de base
+    alphabet = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    index = 0
+    while index < len(alphabet):
+        if alphabet[index] == c:
+            ascii_code = index + 32  # car alphabet commence à l'ASCII 32 (espace)
+            break
+        index += 1
+
+    hex_chars = "0123456789ABCDEF"
+    high = ascii_code // 16
+    low = ascii_code % 16
+    return hex_chars[high] + hex_chars[low]
+
+def send_message(text):
+    hex_msg = ""
+    for i in range(len(text)):
+        c = text[i]
+        hex_msg += char_to_hex(c)
+    serial.write_string("AT+MSGHEX=\"" + hex_msg + "\"\r\n")
 
 # Programme principal
 init_lora()
-display.show('S')  # Send
-
+basic.show_string("S")
 while True:
-    # Exemple : envoyer "Hello"
-    send_message("Hello")
-    display.show(Image.HAPPY)
-    sleep(60000)  # attendre 1 minute
+    send_message("HELLO")  # envoie "HELLO"
+    basic.show_icon(IconNames.HAPPY)
+    basic.pause(60000)           # toutes les 60 secondes
