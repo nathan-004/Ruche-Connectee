e3b6eebbcecb240335b3f8c449570155734758a5
Nathan <148985551+nathan-004@users.noreply.github.com>
2025-05-11 14:20:23 +0200

    Update bluetooth.py

--- Bluetooth/bluetooth.py
+++ Bluetooth/bluetooth.py
@@ -1,8 +1,17 @@
+message = ""
+
+def on_bluetooth_connected():
+    bluetooth.start_uart_service()
+    basic.show_string("C")
+bluetooth.on_bluetooth_connected(on_bluetooth_connected)
+
+def on_bluetooth_disconnected():
+    basic.show_string("D")
+bluetooth.on_bluetooth_disconnected(on_bluetooth_disconnected)
+
 def on_uart_data_received():
-    received = bluetooth.uart_read_until(serial.delimiters(Delimiters.NEW_LINE))
-    serial.write_line(received)
-
-bluetooth.on_uart_data_received(serial.delimiters(Delimiters.NEW_LINE),on_uart_data_received)
-
-bluetooth.start_uart_service()
-basic.show_icon(IconNames.HAPPY)
+    global message
+    message = bluetooth.uart_read_until(serial.delimiters(Delimiters.HASH))
+    bluetooth.uart_write_string(message)
+    basic.show_string(message)
+bluetooth.on_uart_data_received(serial.delimiters(Delimiters.HASH), on_uart_data_received)
